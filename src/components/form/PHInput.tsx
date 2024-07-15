dbad4622ca2aa280c8a9ccfca9fd4e6ce78a2f0a
Nirob_Barman <niroborin111@gmail.com>
2024-07-15 07:50:15 +0600

    Refactor Form Component

--- src/components/form/PHInput.tsx
+++ src/components/form/PHInput.tsx
@@ -1,7 +1,7 @@
-import { Input } from "antd";
+import { Form, Input } from "antd";
 import {
-    Controller,
-    // useFormContext
+  Controller,
+  // useFormContext
 } from "react-hook-form";
 
 type TInputProps = {
@@ -14,13 +14,25 @@ const PHInput = ({ type, name, label }: TInputProps) => {
   //   const { register } = useFormContext();
   // return <input type="text" id="password" {...register("password")} />
   // return <input type={type} id={name} {...register(name)} />
+
   return (
+    // <div style={{ marginBottom: "20px" }}>
+    //   {label ? label : null}
+    //   <Controller
+    //     name={name}
+    //     render={({ field }) => <Input {...field} type={type} id={name} />}
+    //     //   <Input type={type} id={name} {...register(name)} />
+    //   />
+    // </div>
+
     <div style={{ marginBottom: "20px" }}>
-      {label ? label : null}
       <Controller
         name={name}
-        render={({ field }) => <Input {...field} type={type} id={name} />}
-        //   <Input type={type} id={name} {...register(name)} />
+        render={({ field }) => (
+          <Form.Item label={label}>
+            <Input {...field} type={type} id={name} size="large" />
+          </Form.Item>
+        )}
       />
     </div>
   );
--- src/pages/admin/academicManagement/CreateAcademicSemester.tsx
+++ src/pages/admin/academicManagement/CreateAcademicSemester.tsx
@@ -1,20 +1,26 @@
 import { FieldValues, SubmitHandler } from "react-hook-form";
 import PHForm from "../../../components/form/PHForm";
 import PHInput from "../../../components/form/PHInput";
-import { Button } from "antd";
+import { Button, Col, Flex } from "antd";
+import PHSelect from "../../../components/form/PHSelect";
 
 const CreateAcademicSemester = () => {
+  const onSubmit: SubmitHandler<FieldValues> = async (data) => {
+    console.log(data);
+  };
 
-    const onSubmit: SubmitHandler<FieldValues> = async (data) => {
-        console.log(data);
-    }
-
-    return (
-      <PHForm onSubmit={onSubmit}>
-        <PHInput type="text" name="name" />
-        <Button htmlType="submit">Submit</Button>
-      </PHForm>
-    );
+  return (
+    <Flex justify="center" align="center">
+      <Col span={6}>
+        <PHForm onSubmit={onSubmit}>
+          <PHInput type="text" name="name" label="name" />
+          <PHInput type="text" name="name" label="year" />
+          <PHSelect name="drop" label="drop" options={[{ value: "1", label: "1" }]} />
+          <Button htmlType="submit">Submit</Button>
+        </PHForm>
+      </Col>
+    </Flex>
+  );
 };
 
 export default CreateAcademicSemester;
