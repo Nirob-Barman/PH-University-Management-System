f6481a25d3e3eec872c661e696a4dc2ba6c677fb
Nirob_Barman <niroborin111@gmail.com>
2024-07-15 08:05:13 +0600

    Reusable Select Element

--- src/pages/admin/academicManagement/CreateAcademicSemester.tsx
+++ src/pages/admin/academicManagement/CreateAcademicSemester.tsx
@@ -1,21 +1,56 @@
 import { FieldValues, SubmitHandler } from "react-hook-form";
 import PHForm from "../../../components/form/PHForm";
-import PHInput from "../../../components/form/PHInput";
+// import PHInput from "../../../components/form/PHInput";
 import { Button, Col, Flex } from "antd";
 import PHSelect from "../../../components/form/PHSelect";
 
+const nameOptions = [
+  {
+    value: "01",
+    lable: "Autumn",
+  },
+  {
+    value: "02",
+    lable: "Summer",
+  },
+];
+// const codeOptions = [
+//   {
+//     value: "01",
+//     lable: "01",
+//   },
+//   {
+//     value: "02",
+//     lable: "02",
+//   },
+// ];
+
 const CreateAcademicSemester = () => {
   const onSubmit: SubmitHandler<FieldValues> = async (data) => {
-    console.log(data);
+    // console.log(data);
+
+    const semesterData = {
+      name: "Something",
+      code: "Something",
+      //   name,
+      //   code: data.name,
+      //   year: data.year,
+      //   startMonth: data.startMonth,
+      //   endMonth: data.endMonth,
+    };
+
+    console.log(semesterData);
   };
 
   return (
     <Flex justify="center" align="center">
       <Col span={6}>
         <PHForm onSubmit={onSubmit}>
-          <PHInput type="text" name="name" label="name" />
-          <PHInput type="text" name="name" label="year" />
-          <PHSelect name="drop" label="drop" options={[{ value: "1", label: "1" }]} />
+          {/* <PHInput type="text" name="name" label="name" /> */}
+          {/* <PHInput type="text" name="name" label="year" /> */}
+          <PHSelect name="name" label="name" options={nameOptions} />
+          {/* <PHSelect name="code" label="code" options={codeOptions} /> */}
+          {/* <PHSelect name="name" label="name" options={[{ value: "1", label: "1" }]} /> */}
           <Button htmlType="submit">Submit</Button>
         </PHForm>
       </Col>
