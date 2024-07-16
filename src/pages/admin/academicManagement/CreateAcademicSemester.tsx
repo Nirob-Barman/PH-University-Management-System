d506afca695db21d69be98f30c36b1206b19e77b
Nirob_Barman <niroborin111@gmail.com>
2024-07-16 07:37:15 +0600

    Handle Create Academic Semester Form

--- src/pages/admin/academicManagement/CreateAcademicSemester.tsx
+++ src/pages/admin/academicManagement/CreateAcademicSemester.tsx
@@ -7,34 +7,49 @@ import PHSelect from "../../../components/form/PHSelect";
 const nameOptions = [
   {
     value: "01",
-    lable: "Autumn",
+    label: "Autumn",
   },
   {
     value: "02",
-    lable: "Summer",
+    label: "Summer",
   },
 ];
 // const codeOptions = [
 //   {
 //     value: "01",
-//     lable: "01",
+//     label: "01",
 //   },
 //   {
 //     value: "02",
-//     lable: "02",
+//     label: "02",
 //   },
 // ];
+
+const currentYear = new Date().getFullYear();
+// console.log(currentYear);
+
+const yearOptions = [0, 1, 2, 3, 4].map((number) => ({
+  value: String(currentYear + number),
+  label: String(currentYear + number),
+}));
+// console.log(yearOptions);
 
 const CreateAcademicSemester = () => {
   const onSubmit: SubmitHandler<FieldValues> = async (data) => {
     // console.log(data);
+    console.log(data.name);
+
+    const name = nameOptions[Number(data.name) -1].label;
 
     const semesterData = {
-      name: "Something",
-      code: "Something",
-      //   name,
-      //   code: data.name,
-      //   year: data.year,
+      // name: "Something",
+      // name: name,
+      // name,
+      // code: "Something",
+      // code: data.name,
+        name,
+        code: data.name,
+        year: data.year,
       //   startMonth: data.startMonth,
       //   endMonth: data.endMonth,
     };
@@ -51,6 +66,13 @@ const CreateAcademicSemester = () => {
           <PHSelect name="name" label="name" options={nameOptions} />
           {/* <PHSelect name="code" label="code" options={codeOptions} /> */}
           {/* <PHSelect name="name" label="name" options={[{ value: "1", label: "1" }]} /> */}
+          <PHSelect name="Year" label="year" options={yearOptions} />
+          <PHSelect
+            name="Start month"
+            label="startMonth"
+            options={nameOptions}
+          />
+          <PHSelect name="End month" label="endMonth" options={nameOptions} />
           <Button htmlType="submit">Submit</Button>
         </PHForm>
       </Col>
