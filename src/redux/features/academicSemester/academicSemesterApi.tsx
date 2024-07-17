40a4adc76bca30c943aec843f353bc3e5a8d2d12
Nirob_Barman <niroborin111@gmail.com>
2024-07-17 08:17:53 +0600

    Generic Type For Response Data And Query Params Recap

--- src/redux/features/academicSemester/academicSemesterApi.tsx
+++ src/redux/features/academicSemester/academicSemesterApi.tsx
@@ -1,14 +1,33 @@
-import { TResponseRedux } from "../../../types";
+import { TQueryParam, TResponseRedux } from "../../../types";
 import { TAcademicSemester } from "../../../types/academicManagement.type";
 import { baseApi } from "../../api/baseApi";
 
 const academicSemesterApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
     getAllSemesters: builder.query({
-      query: () => ({
-        url: "/academic-semesters",
-        method: "GET",
-      }),
+      // query: () => ({
+      //   url: "/academic-semesters",
+      //   method: "GET",
+      //   // params: useParams,
+      // }),
+      query: (args) => {
+        console.log(args);
+        const params = new URLSearchParams();
+        // params.append('name', 'Fall')
+        // params.append('name', args)
+
+        // if (args) {
+        //   args.forEach((item: TQueryParam) => {
+        //     params.append(item.name, item.value as string);
+        //   });
+        // }
+
+        return {
+          url: "/academic-semesters",
+          method: "GET",
+          params: params,
+        };
+      },
       transformResponse: (response: TResponseRedux<TAcademicSemester[]>) => {
         // console.log("Inside transform ", response);
         // return response;
