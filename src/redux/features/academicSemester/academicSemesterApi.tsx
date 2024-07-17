543ebc9cb951b38d75eac2c2177e28ac0837bbb4
Nirob_Barman <niroborin111@gmail.com>
2024-07-17 07:57:11 +0600

    Show Semester Data Using Ant Design Table

--- src/redux/features/academicSemester/academicSemesterApi.tsx
+++ src/redux/features/academicSemester/academicSemesterApi.tsx
@@ -1,3 +1,5 @@
+import { TAcademicSemester } from "../../../types/academicManagement.type";
+import { TResponseRedux } from "../../../types/global";
 import { baseApi } from "../../api/baseApi";
 
 const academicSemesterApi = baseApi.injectEndpoints({
@@ -7,6 +9,15 @@ const academicSemesterApi = baseApi.injectEndpoints({
         url: "/academic-semesters",
         method: "GET",
       }),
+      transformResponse: (response: TResponseRedux<TAcademicSemester[]>) => {
+        // console.log("Inside transform ", response);
+        // return response;
+        // console.log("Meta",response.meta);
+        return {
+          data: response.data,
+          meta: response.meta,
+        };
+      },
     }),
   }),
 });
